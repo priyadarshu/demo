24d02c77fc25787456bcbab25eaadef89aafd482
priyadarshu <priyadarshinichettiar@gmail.com>
2021-03-25 21:43:14 +0530

    added husky details

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-module.exports = {
-	extends: ["airbnb"],
-	rules: {
-		"react/jsx-filename-extension": "off",
-		//You can override any rules you want
-	},
+//export const extends = ["airbnb"];
+export const rules = {
+    "react/jsx-filename-extension": "off",
+    //You can override any rules you want
 };
